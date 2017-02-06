8613100beb62df88a9ac37a989783ce492c1c663
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2017-02-06 22:43:19 +0700

    initial commit

--- cheatlist.lua
+++ cheatlist.lua
@@ -13,34 +13,31 @@ c_give("quiver") -- zquiver
 
 c_give("flint") -- "arrow"
 c_give("nitre") -- "coldfire"
+c_give("silk") -- "fishingrod"
 c_give("charcoal") -- "meatrack"
-c_give("houndstooth") -- "trap_teeth"
 
 c_give("cane")
-c_give("heatrock")
-c_give("fishingrod")
-
 c_give("molehat")
+c_give("panflute")
 c_give("beefalohat")
 c_give("eyebrellahat")
 
+c_give("trap_teeth")
 c_give("turf_woodfloor")
 c_give("mech_stone_item")
 c_give("wall_stone_item")
 
 c_spawn("icebox")
-c_spawn("fast_farmplot")
-
-c_spawn("pighouse")
-
+c_spawn("rainometer")
 c_spawn("path_light")
 c_spawn("coldfirepit")
 c_spawn("wood_shelter")
-
-c_spawn("rainometer")
 c_spawn("winterometer")
 c_spawn("lightning_rod")
+c_spawn("fast_farmplot")
 c_spawn("firesuppressor")
+
+c_spawn("pighouse")
 
 --------------------------------------------------------------------------------
 
