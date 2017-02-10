f97995640e6c81a37ee54435b32d0b0aaefe80b3
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2017-02-11 04:01:32 +0700

    initial commit

--- Mods/Patch/modmain.lua
+++ Mods/Patch/modmain.lua
@@ -13,6 +13,16 @@
 
 --Recipe("bedroll_furry", {Ingredient("bedroll_straw", 1), Ingredient("beefalowool", 4)}, RECIPETABS.SURVIVAL, TECH.SCIENCE_ONE)
 
+------------------------------------------------------
+
+Assets =
+{
+	Asset("ATLAS", "images/newslots.xml"),
+
+}
+
+------------------------------------------------------
+
 TUNING.BIRD_SPAWN_MAX = 5
 TUNING.BIRD_SPAWN_DELAY = {min=3, max=12}
 
@@ -21,6 +31,54 @@ TUNING.PERISH_FRIDGE_MULT = .1
 TUNING.TRAP_TEETH_USES = 10
 TUNING.TRAP_TEETH_DAMAGE = 150
 
+------------------------------------------------------
+
 AddPrefabPostInit("firesuppressor", function(inst)
     table.insert(inst.components.firedetector.NOTAGS, "campfire")
+end)
+
+------------------------------------------------------
+
+function playerpostinit(inst)
+	inst:AddComponent("pickyfeet")
+end
+
+AddSimPostInit(playerpostinit)
+
+------------------------------------------------------
+
+function backpackpostinit(inst)
+	inst.components.equippable.equipslot = GLOBAL.EQUIPSLOTS.BACK
+end
+
+function inventorypostinit(component,inst)
+	inst.components.inventory.numequipslots = 5
+end
+
+AddComponentPostInit("inventory", inventorypostinit)
+
+AddPrefabPostInit("backpack", backpackpostinit)
+AddPrefabPostInit("piggyback", backpackpostinit)
+AddPrefabPostInit("krampus_sack", backpackpostinit)
+
+AddSimPostInit(gamepostinit)
+
+table.insert(GLOBAL.EQUIPSLOTS, "BACK")
+GLOBAL.EQUIPSLOTS.BACK = "back"
+
+AddClassPostConstruct("screens/playerhud", function(self) 
+    local oldfn = self.SetMainCharacter
+    function self:SetMainCharacter(maincharacter)
+        
+        oldfn(self, maincharacter)
+        
+        self.controls.inv:AddEquipSlot(GLOBAL.EQUIPSLOTS.BACK, "images/newslots.xml", "back.tex") 
+        self.controls.inv.bg:SetScale(1.2,1,1)
+
+        local bp = maincharacter.components.inventory:GetEquippedItem(GLOBAL.EQUIPSLOTS.BACK)
+        if bp and bp.components.container then
+            bp.components.container:Close()
+            bp.components.container:Open(maincharacter)
+        end
+    end
 end)
---
+++ Mods/Patch/scripts/components/pickyfeet.lua
@@ -0,0 +1,32 @@
+local PickyFeet= Class(function(self, inst)
+    self.inst = inst
+	self.old_night_mul = inst.components.sanity.night_drain_mult
+	self.old_inherentinsulation = inst.components.temperature.inherentinsulation
+	self.task = self.inst:DoPeriodicTask(0.5, function() self:OnUpdate(0.5) end, 0)
+end)
+
+function getFloor(self)
+	local x,y,z = self.inst.Transform:GetWorldPosition()
+	local ground = GetWorld()
+	if ground then
+		local tile = ground.Map:GetTileAtPoint(x,y,z)
+		return tile
+	end
+end
+
+function Wood(self)
+	local tile = getFloor(self)
+	if tile==GROUND.WOODFLOOR then
+		self.inst.components.sanity.night_drain_mult=0.1
+		self.inst.components.temperature.inherentinsulation=TUNING.INSULATION_SMALL	
+	else
+		self.inst.components.sanity.night_drain_mult=self.old_night_mul
+		self.inst.components.temperature.inherentinsulation=self.old_inherentinsulation
+	end
+end
+
+function PickyFeet:OnUpdate(dt)
+	Wood(self)
+end
+
+return PickyFeet
