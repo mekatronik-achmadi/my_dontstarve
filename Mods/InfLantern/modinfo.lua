e0e785bfc990c280b68912468aeaf67a96075c8d
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2017-02-27 13:42:44 +0700

    initial commit

---
+++ Mods/InfLantern/modinfo.lua
@@ -0,0 +1,16 @@
+
+name = "Infinite Lantern"
+description = "One Light Solution"
+author = "Achmadi"
+version = "1.1"
+
+forumthread = ""
+
+api_version = 6
+
+dont_starve_compatible = true
+reign_of_giants_compatible = true
+shipwrecked_compatible = false
+
+icon_atlas = "inf_lantern.xml"
+icon = "inf_lantern.tex"
