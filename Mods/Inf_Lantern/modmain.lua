b08d3621e0e50d6cbe006472081164b0eac668c2
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2017-02-28 15:03:36 +0700

    initial commit

--- Mods/Inf_Lantern/modmain.lua
+++ Mods/Inf_Lantern/modmain.lua
@@ -19,4 +19,4 @@ STRINGS.RECIPE_DESC.INF_LANTERN = "One Light Soulution"
 GLOBAL.STRINGS.CHARACTERS.GENERIC.DESCRIBE.PATH_LIGHT = "A useful lantern"
 
 local inf_lantern = Recipe("inf_lantern", {Ingredient("twigs", 3),Ingredient("rope", 1),Ingredient("goldnugget", 1)}, RECIPETABS.LIGHT,  TECH.NONE)
-inf_lantern.atlas = "images/inventoryimages/path_light.xml"
+inf_lantern.atlas = "images/inventoryimages/inf_lantern.xml"
