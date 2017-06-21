359cc278e4ab5a87138f227626746e5cea2a25ef
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2017-06-21 21:02:00 +0700

    initial commit

--- Mods/Inf_Lantern/modmain.lua
+++ Mods/Inf_Lantern/modmain.lua
@@ -15,7 +15,7 @@ Ingredient = GLOBAL.Ingredient
 TECH = GLOBAL.TECH
 
 GLOBAL.STRINGS.NAMES.INF_LANTERN = "Inf Lantern"
-STRINGS.RECIPE_DESC.INF_LANTERN = "One Light Soulution"
+STRINGS.RECIPE_DESC.INF_LANTERN = "One Light Solution"
 GLOBAL.STRINGS.CHARACTERS.GENERIC.DESCRIBE.PATH_LIGHT = "A useful lantern"
 
 local inf_lantern = Recipe("inf_lantern", {Ingredient("twigs", 3),Ingredient("rope", 1),Ingredient("goldnugget", 1)}, RECIPETABS.LIGHT,  TECH.NONE)
