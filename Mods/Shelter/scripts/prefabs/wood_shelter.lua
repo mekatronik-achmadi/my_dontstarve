dcb7d76fd7de45228046f23c1973f3688586737a
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2017-07-28 17:20:42 +0700

    initial commit

--- Mods/Shelter/scripts/prefabs/wood_shelter.lua
+++ Mods/Shelter/scripts/prefabs/wood_shelter.lua
@@ -33,6 +33,113 @@ local function onfar(inst)
        TUNING.WOOD_SHELTER_SLEEPING = 0
 end
 
+local function onsleep(inst, sleeper)
+  
+  if GetClock():IsDay() then
+		local tosay = "ANNOUNCE_NODAYSLEEP"
+		if GetWorld():IsCave() then
+			tosay = "ANNOUNCE_NODAYSLEEP_CAVE"
+		end
+		if sleeper.components.talker then
+			sleeper.components.talker:Say(GetString(sleeper.prefab, tosay))
+			return
+		end
+	end
+  
+  if inst:HasTag("fire") then
+		if sleeper.components.talker then
+			sleeper.components.talker:Say(GetString(sleeper.prefab, "ANNOUNCE_NOSLEEPONFIRE"))
+		end
+		return
+	end
+  
+  local hounded = GetWorld().components.hounded
+
+	local danger = FindEntity(inst, 10, function(target) 
+		return
+			(target:HasTag("monster") and not target:HasTag("player") and not sleeper:HasTag("spiderwhisperer"))
+			or (target:HasTag("monster") and not target:HasTag("player") and sleeper:HasTag("spiderwhisperer") and not target:HasTag("spider"))
+			or (target:HasTag("pig") and not target:HasTag("player") and sleeper:HasTag("spiderwhisperer"))
+			or (target.components.combat and target.components.combat.target == sleeper)
+	end)
+	
+	if hounded and (hounded.warning or hounded.timetoattack <= 0) then
+		danger = true
+	end
+	
+	if danger then
+		if sleeper.components.talker then
+			sleeper.components.talker:Say(GetString(sleeper.prefab, "ANNOUNCE_NODANGERSLEEP"))
+		end
+		return
+	end
+  
+  if sleeper.components.hunger.current < TUNING.CALORIES_MED then
+		sleeper.components.talker:Say(GetString(sleeper.prefab, "ANNOUNCE_NOHUNGERSLEEP"))
+		return
+	end
+  
+  sleeper.components.health:SetInvincible(true)
+	sleeper.components.playercontroller:Enable(false)
+
+	GetPlayer().HUD:Hide()
+	TheFrontEnd:Fade(false,1)
+  
+  inst:DoTaskInTime(1.2, function() 
+    GetPlayer().HUD:Show()
+    TheFrontEnd:Fade(true,1) 
+    
+    if GetClock():IsDay() then
+      local tosay = "ANNOUNCE_NODAYSLEEP"
+			if GetWorld():IsCave() then
+				tosay = "ANNOUNCE_NODAYSLEEP_CAVE"
+			end
+      
+      if sleeper.components.talker then				
+				sleeper.components.talker:Say(GetString(sleeper.prefab, tosay))
+				sleeper.components.health:SetInvincible(false)
+				sleeper.components.playercontroller:Enable(true)
+				return
+			end
+        
+    end
+    
+    if sleeper.components.sanity then
+			sleeper.components.sanity:DoDelta(TUNING.SANITY_HUGE)
+		end
+		
+		if sleeper.components.hunger then
+			sleeper.components.hunger:DoDelta(-TUNING.CALORIES_HUGE, false, true)
+		end
+		
+		if sleeper.components.health then
+			sleeper.components.health:DoDelta(TUNING.HEALING_HUGE, false, "tent", true)
+		end
+    
+    if sleeper.components.temperature and sleeper.components.temperature.current < TUNING.TARGET_SLEEP_TEMP then
+			sleeper.components.temperature:SetTemperature(TUNING.TARGET_SLEEP_TEMP)
+		end		
+		
+		local moisture_start = nil
+		if sleeper.components.moisture and sleeper.components.moisture:GetMoisture() > 0 then
+			moisture_start = sleeper.components.moisture.moisture
+		end
+    
+    GetClock():MakeNextDay()
+    
+    if moisture_start then
+			sleeper.components.moisture.moisture = moisture_start - TUNING.SLEEP_MOISTURE_DELTA
+			if sleeper.components.moisture.moisture < 0 then sleeper.components.moisture.moisture = 0 end
+		end
+		
+		sleeper.components.health:SetInvincible(false)
+		sleeper.components.playercontroller:Enable(true)
+		sleeper.sg:GoToState("wakeup")	
+    
+  end)
+
+end
+
 local function fn(Sim)
 	local inst = CreateEntity()
 	local trans = inst.entity:AddTransform()
@@ -47,7 +154,6 @@ local function fn(Sim)
 	minimap:SetIcon( "siesta_shelter.tex" )
 
 	inst:AddTag("shelter")
-	MakeSnowCovered(inst, .01)
 	
 	inst:AddComponent( "playerprox" )
 	inst.components.playerprox:SetOnPlayerNear(onnear)    
@@ -66,6 +172,13 @@ local function fn(Sim)
 	inst.components.workable:SetOnWorkCallback(onhit)
 
 	inst:ListenForEvent( "onbuilt", onbuilt)
+  
+  inst:AddComponent("sleepingbag")
+	inst.components.sleepingbag.onsleep = onsleep
+  
+  MakeSnowCovered(inst, .01)
+  MakeLargeBurnable(inst, nil, nil, true)
+	MakeLargePropagator(inst)
 	
     return inst
 end
