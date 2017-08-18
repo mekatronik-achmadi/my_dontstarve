ea40ab80ed76e6ec87dc2faadca1c115006b96b8
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2017-08-19 01:13:33 +0700

    initial commit

--- Mods/Patch/modmain.lua
+++ Mods/Patch/modmain.lua
@@ -22,6 +22,7 @@ Recipe("razor", {Ingredient("twigs", 2), Ingredient("flint", 2)}, RECIPETABS.TOO
 Recipe("shovel", {Ingredient("twigs", 2),Ingredient("flint", 2)}, RECIPETABS.TOOLS,  TECH.NONE)
 Recipe("pitchfork", {Ingredient("twigs", 2),Ingredient("flint", 2)}, RECIPETABS.TOOLS,  TECH.NONE)
 Recipe("hammer", {Ingredient("twigs", 2),Ingredient("rocks", 2), Ingredient("rope", 1)}, RECIPETABS.TOOLS, TECH.NONE)
+Recipe("cane", {Ingredient("goldnugget", 1), Ingredient("houndstooth", 6), Ingredient("twigs", 4)}, RECIPETABS.TOOLS,  TECH.NONE)
 
 -- Light
 Recipe("torch", {Ingredient("cutgrass", 2),Ingredient("twigs", 1)}, RECIPETABS.LIGHT, TECH.NONE)
@@ -32,6 +33,7 @@ Recipe("coldfirepit", {Ingredient("nitre", 2), Ingredient("cutgrass", 3),Ingredi
 
 -- Survival
 Recipe("trap", {Ingredient("twigs", 2),Ingredient("cutgrass", 4)}, RECIPETABS.SURVIVAL, TECH.NONE)
+Recipe("birdtrap", {Ingredient("twigs", 6),Ingredient("silk", 4)}, RECIPETABS.SURVIVAL, TECH.NONE)
 Recipe("horn", {Ingredient("log", 1),Ingredient("houndstooth", 4)}, RECIPETABS.SURVIVAL, TECH.NONE)
 Recipe("fishingrod", {Ingredient("twigs", 2),Ingredient("silk", 1)}, RECIPETABS.SURVIVAL, TECH.NONE)
 Recipe("backpack", {Ingredient("cutgrass", 4), Ingredient("twigs", 4)}, RECIPETABS.SURVIVAL, TECH.NONE)
@@ -66,6 +68,7 @@ Recipe("wall_stone_item", {Ingredient("cutstone", 2)}, RECIPETABS.TOWN, TECH.NON
 Recipe("treasurechest", {Ingredient("boards", 3)}, RECIPETABS.TOWN, TECH.NONE, "treasurechest_placer",1)
 Recipe("spidereggsack", {Ingredient("silk", 4), Ingredient("monstermeat", 2),Ingredient("spidergland", 2)}, RECIPETABS.TOWN, TECH.NONE)
 Recipe("pighouse", {Ingredient("boards", 4), Ingredient("cutstone", 2), Ingredient("pigskin", 2)}, RECIPETABS.TOWN, TECH.NONE, "pighouse_placer")
+Recipe("birdcage", {Ingredient("cutgrass", 6), Ingredient("goldnugget", 2), Ingredient("boards", 2)}, RECIPETABS.TOWN, TECH.NONE, "birdcage_placer")
 Recipe("rabbithouse", {Ingredient("boards", 4), Ingredient("cutstone", 2), Ingredient("manrabbit_tail", 2)}, RECIPETABS.TOWN, TECH.NONE, "rabbithouse_placer")
 
 -- Refine
