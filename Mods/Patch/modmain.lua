e79403d42ae6d19524134dcdd78f5fdc7539bd21
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2017-08-30 12:40:08 +0700

    initial commit

--- Mods/Patch/modmain.lua
+++ Mods/Patch/modmain.lua
@@ -29,6 +29,7 @@ Recipe("cane", {Ingredient("goldnugget", 1), Ingredient("houndstooth", 6), Ingre
 Recipe("torch", {Ingredient("cutgrass", 2),Ingredient("twigs", 1)}, RECIPETABS.LIGHT, TECH.NONE)
 Recipe("campfire", {Ingredient("cutgrass", 3), Ingredient("log", 1)}, RECIPETABS.LIGHT, TECH.NONE, "campfire_placer")
 Recipe("coldfire", {Ingredient("cutgrass", 3), Ingredient("nitre", 1)}, RECIPETABS.LIGHT, TECH.NONE, "coldfire_placer")
+Recipe("lantern", {Ingredient("twigs", 3), Ingredient("rope", 2), Ingredient("lightbulb", 2)}, RECIPETABS.LIGHT, TECH.NONE)
 Recipe("firepit", {Ingredient("log", 2), Ingredient("cutgrass", 3),Ingredient("rocks", 8)}, RECIPETABS.LIGHT, TECH.NONE, "firepit_placer")
 Recipe("coldfirepit", {Ingredient("nitre", 2), Ingredient("cutgrass", 3),Ingredient("rocks", 8)}, RECIPETABS.LIGHT, TECH.NONE, "coldfirepit_placer")
 
@@ -39,10 +40,12 @@ Recipe("horn", {Ingredient("log", 1),Ingredient("houndstooth", 4)}, RECIPETABS.S
 Recipe("fishingrod", {Ingredient("twigs", 2),Ingredient("silk", 1)}, RECIPETABS.SURVIVAL, TECH.NONE)
 Recipe("backpack", {Ingredient("cutgrass", 4), Ingredient("twigs", 4)}, RECIPETABS.SURVIVAL, TECH.NONE)
 Recipe("bedroll_straw", {Ingredient("cutgrass", 3), Ingredient("rope", 1)}, RECIPETABS.SURVIVAL, TECH.NONE)
+Recipe("bugnet", {Ingredient("twigs", 4), Ingredient("silk", 2), Ingredient("rope", 1)}, RECIPETABS.SURVIVAL, TECH.NONE)
 Recipe("heatrock", {Ingredient("rocks", 5),Ingredient("pickaxe", 1),Ingredient("flint", 2)}, RECIPETABS.SURVIVAL, TECH.NONE)
 Recipe("umbrella", {Ingredient("twigs", 4) ,Ingredient("pigskin", 1), Ingredient("silk",2 )}, RECIPETABS.SURVIVAL, TECH.NONE)
 
 -- Food
+Recipe("beebox", {Ingredient("boards", 2),Ingredient("bee", 4)}, RECIPETABS.FARM, TECH.NONE, "beebox_placer")
 Recipe("meatrack", {Ingredient("twigs", 3),Ingredient("charcoal", 2), Ingredient("rope", 3)}, RECIPETABS.FARM, TECH.NONE, "meatrack_placer")
 Recipe("cookpot", {Ingredient("cutstone", 2),Ingredient("charcoal", 4), Ingredient("twigs", 6)}, RECIPETABS.FARM,  TECH.NONE, "cookpot_placer")
 Recipe("icebox", {Ingredient("goldnugget", 1), Ingredient("gears", 1), Ingredient("cutstone", 1)}, RECIPETABS.FARM,  TECH.NONE, "icebox_placer", 1.5)
@@ -51,6 +54,7 @@ Recipe("fast_farmplot", {Ingredient("boards", 1),Ingredient("poop", 2),Ingredien
 -- Science
 Recipe("gears", {Ingredient("log", 1), Ingredient("rocks", 2)}, RECIPETABS.SCIENCE,  TECH.NONE)
 Recipe("transistor", {Ingredient("goldnugget", 1), Ingredient("rocks", 2)}, RECIPETABS.SCIENCE, TECH.NONE)
+Recipe("gunpowder", {Ingredient("rottenegg", 1), Ingredient("charcoal", 1), Ingredient("nitre", 1)}, RECIPETABS.SCIENCE,  TECH.NONE)
 Recipe("winterometer", {Ingredient("boards", 2), Ingredient("goldnugget", 2)}, RECIPETABS.SCIENCE,  TECH.NONE, "winterometer_placer")
 Recipe("lightning_rod", {Ingredient("goldnugget", 2), Ingredient("cutstone", 1)}, RECIPETABS.SCIENCE,  TECH.NONE, "lightning_rod_placer")
 Recipe("rainometer", {Ingredient("boards", 2), Ingredient("goldnugget", 2), Ingredient("rope",2)}, RECIPETABS.SCIENCE,  TECH.NONE, "rainometer_placer")
@@ -61,13 +65,18 @@ Recipe("resurrectionstatue", {Ingredient("boards", 4),Ingredient("meat", 4),Ingr
 
 -- Fight
 Recipe("armorwood", {Ingredient("log", 4),Ingredient("rope", 1)}, RECIPETABS.WAR,  TECH.NONE)
+Recipe("boomerang", {Ingredient("boards", 1),Ingredient("rope", 1)}, RECIPETABS.WAR,  TECH.NONE)
+Recipe("spear", {Ingredient("twigs", 2),Ingredient("rope", 1),Ingredient("flint", 1) }, RECIPETABS.WAR,  TECH.NONE)
+Recipe("footballhat", {Ingredient("pigskin", 1), Ingredient("rope", 1), Ingredient("log", 1)}, RECIPETABS.WAR,  TECH.NONE)
 Recipe("trap_teeth", {Ingredient("log", 1),Ingredient("rope", 1),Ingredient("houndstooth", 1)}, RECIPETABS.WAR,  TECH.NONE)
 
 -- Structure
 Recipe("turf_woodfloor", {Ingredient("boards", 1)}, RECIPETABS.TOWN, TECH.NONE)
 Recipe("treasurechest", {Ingredient("boards", 3)}, RECIPETABS.TOWN, TECH.NONE, "treasurechest_placer",1)
 Recipe("wall_wood_item", {Ingredient("log", 3), Ingredient("rope", 1)}, RECIPETABS.TOWN, TECH.NONE,nil,nil,nil,6)
+Recipe("wall_hay_item", {Ingredient("cutgrass", 3), Ingredient("rope", 1) }, RECIPETABS.TOWN, TECH.NONE,nil,nil,nil,6)
 Recipe("spidereggsack", {Ingredient("silk", 4), Ingredient("monstermeat", 2),Ingredient("spidergland", 2)}, RECIPETABS.TOWN, TECH.NONE)
+Recipe("mermhouse", {Ingredient("boards", 4), Ingredient("cutstone", 2), Ingredient("fish", 2)}, RECIPETABS.TOWN, TECH.NONE, "pighouse_placer")
 Recipe("pighouse", {Ingredient("boards", 4), Ingredient("cutstone", 2), Ingredient("pigskin", 2)}, RECIPETABS.TOWN, TECH.NONE, "pighouse_placer")
 Recipe("birdcage", {Ingredient("cutgrass", 6), Ingredient("goldnugget", 2), Ingredient("boards", 2)}, RECIPETABS.TOWN, TECH.NONE, "birdcage_placer")
 Recipe("rabbithouse", {Ingredient("boards", 4), Ingredient("cutstone", 2), Ingredient("manrabbit_tail", 2)}, RECIPETABS.TOWN, TECH.NONE, "rabbithouse_placer")
@@ -78,7 +87,8 @@ Recipe("rope", {Ingredient("cutgrass", 3)}, RECIPETABS.REFINE,  TECH.NONE)
 Recipe("cutstone", {Ingredient("rocks", 3)}, RECIPETABS.REFINE,  TECH.NONE)
 
 -- Dress
-Recipe("molehat", {Ingredient("mole", 1), Ingredient("transistor", 1)}, RECIPETABS.DRESS,  TECH.NONE)
+Recipe("strawhat", {Ingredient("cutgrass", 8)}, RECIPETABS.DRESS,  TECH.NONE)
+Recipe("beehat", {Ingredient("silk", 4), Ingredient("strawhat", 1)}, RECIPETABS.DRESS,  TECH.NONE)
 Recipe("beefalohat", {Ingredient("beefalowool", 4),Ingredient("horn", 1)}, RECIPETABS.DRESS,  TECH.NONE)
 
 ------------------------------------------------------
@@ -86,6 +96,7 @@ Recipe("beefalohat", {Ingredient("beefalowool", 4),Ingredient("horn", 1)}, RECIP
 STRINGS = GLOBAL.STRINGS
 
 STRINGS.RECIPE_DESC.HORN = "Sound the call"
+STRINGS.RECIPE_DESC.MERMHOUSE = "Create a war"
 STRINGS.RECIPE_DESC.SPIDEREGGSACK = "Plant a fight club"
 
 ------------------------------------------------------
